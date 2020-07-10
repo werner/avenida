ce23d4ea7973d3f1f7eb84b6deb05bd0c37f1522
werner <werner_a_e@yahoo.es>
2020-07-10 12:09:40 -0300

    Add endpoint to handle the solution

---
+++ endpoint.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'net/http'
+require 'json'
+require './board'
+
+get '/solve' do
+  uri = URI('https://mine-sweeper-generator.herokuapp.com/solver')
+
+  response = Net::HTTP.get_response(uri)
+  if response.is_a?(Net::HTTPSuccess)
+    board = Board.new(JSON.parse(response.body))
+    {
+      problem: response.body,
+      solution: board.solve.to_s
+    }.to_s
+  else
+    {
+      error: "Network Error"
+    }.to_s
+  end
+end
